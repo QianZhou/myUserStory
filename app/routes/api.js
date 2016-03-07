29087ee1271c51e3c65bd6a2fedb757ff5252b95
QianZhou <qzhou@lamar.edu>
2016-03-07 00:28:45 -0600

    fix signup bugs

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,129 +1,147 @@
+var User = require('../models/user');
 
-var User=require('../models/user');
+var Story = require('../models/story');
 
-var Story=require('../models/story');
+var config = require('../../config');
 
-var config=require('../../config');
+var secretKey = config.secretKey;
 
-var secretKey=config.secretKey;
+var jsonwebtoken = require('jsonwebtoken');
 
-var jsonwebtoken=require('jsonwebtoken');
-
-function createToken(user){
-	var token=jsonwebtoken.sign({
-		id:user._id,
-		name:user.name,
-		username:user.username
-	},secretKey,{
-		expirtesInMinute:1440
-	});
-	return token;
+function createToken(user) {
+    var token = jsonwebtoken.sign({
+        id: user._id,
+        name: user.name,
+        username: user.username
+    }, secretKey, {
+        expirtesInMinute: 1440
+    });
+    return token;
 }
 
-module.exports=function(app,express){
-	var api=express.Router();
+module.exports = function (app, express) {
+    var api = express.Router();
 
-	api.post('/signup',function(req,res){
-		var user=new User({
-			name:req.body.name,
-			username:req.body.username,
-			password:req.body.password
-		});
+    api.post('/signup', function (req, res) {
+        var user = new User({
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password
+        });
 
-		user.save(function(err){
-			if(err){
-				res.send(err);
-				return;
-			}
-			res.json({message:'User has been created!'});
-		});
-	});
+        var token = createToken(user);
+        user.save(function (err) {
+            if (err) {
+                res.send(err);
+                return;
+            }
+            res.json({
+                success: true,
+                message: 'User has been created!',
+                token: token
+            });
+        });
+    });
 
-	api.get('/users',function(req,res){
-		User.find({},function(err,users){
-			if(err){
-				res.send(err);
-				return;
-			}
-			res.json(users);
-		});
-	});
+    api.get('/users', function (req, res) {
+        User.find({}, function (err, users) {
+            if (err) {
+                res.send(err);
+                return;
+            }
+            res.json(users);
+        });
+    });
 
-	api.post('/login',function(req,res){
-		User.findOne({
-			username:req.body.username
-		}).select('password').exec(function(err,user){
-			if(err) throw err;
-			if(!user){
-				res.send({message:"User does not exist."});
-			}else{
-				var validPassword=user.comparePassword(req.body.password);
-				if(!validPassword){
-					res.send({message:"Invalid password"});
-				}else{
-					var token=createToken(user);
-					res.json({
-						success:true,
-						message:"Successfully login!",
-						token:token
-					});
-				}
-			}
-		});
+    api.post('/login', function (req, res) {
+        User.findOne({
+            username: req.body.username
+        }).select('name username password').exec(function (err, user) {
+            if (err) throw err;
+            if (!user) {
+                res.send({
+                    message: "User does not exist."
+                });
+            } else {
+                var validPassword = user.comparePassword(req.body.password);
+                if (!validPassword) {
+                    res.send({
+                        message: "Invalid password"
+                    });
+                } else {
+                    var token = createToken(user);
+                    res.json({
+                        success: true,
+                        message: "Successfully login!",
+                        token: token
+                    });
+                }
+            }
+        });
 
-		
-	});
+
+    });
 
 
-	api.use(function(req,res,next){
-		console.log("Somebody just came to our app!");
+    api.use(function (req, res, next) {
+        console.log("Somebody just came to our app!");
 
-		var token=req.body.token||req.param('token')||req.headers['x-access-token'];
+        var token = req.body.token || req.param('token') || req.headers['x-access-token'];
 
-		if(token){
-			jsonwebtoken.verify(token,secretKey,function(err,decoded){
-				if(err){
-					res.status(403).send({success:false,message:"Failed to authenticate user!"});
-				}else{
-					req.decoded=decoded;
-					next();
-				}
-			});
-		}else{
-			res.status(403).send({success:false,message:"No token provided"});
-		}
-	});
+        if (token) {
+            jsonwebtoken.verify(token, secretKey, function (err, decoded) {
+                if (err) {
+                    res.status(403).send({
+                        success: false,
+                        message: "Failed to authenticate user!"
+                    });
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        } else {
+            res.status(403).send({
+                success: false,
+                message: "No token provided"
+            });
+        }
+    });
 
-api.route('/')
-	.post(function(req,res){
-		var story=new Story({
-			creator:req.decoded.id,
-			content:req.body.content,
+    api.route('/')
+        .post(function (req, res) {
+            var story = new Story({
+                creator: req.decoded.id,
+                content: req.body.content,
 
-		});
-		story.save(function(err){
-			if(err){
-				res.send(err);
-				return;
-			}
+            });
+            story.save(function (err) {
+                if (err) {
+                    res.send(err);
+                    return;
+                }
 
-			res.json({message:"New Story Created!"});
-		});
-	})
+                res.json({
+                    message: "New Story Created!"
+                });
+            });
+        })
 
-	.get(function(req,res){
-		Story.find({creator:req.decoded.id},function(err,stories){
-			if(err){
-				res.send(err);
-				return;
-			}
-			res.json(stories);
-		});
-	});
- 
-api.get('/me',function(req,res){
-	res.json(req.decoded);
-});
+    .get(function (req, res) {
+        Story.find({
+            creator: req.decoded.id
+        }, function (err, stories) {
+            if (err) {
+                res.send(err);
+                return;
+            }
+            res.json(stories);
+        });
+    });
 
-	return api
+    api.get('/me', function (req, res) {
+        res.json(req.decoded);
+    });
+
+    return api
 }
